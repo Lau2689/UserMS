5e867988e58d49cb10b6c013ef3a31fed2de23c0
Laura <lgal.martin@gmail.com>
2023-04-20 17:42:39 +0200

    Controller Test and controller

--- src/main/java/usermicroservice/controllers/UserController.java
+++ src/main/java/usermicroservice/controllers/UserController.java
@@ -1,5 +1,6 @@
 package usermicroservice.controllers;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import usermicroservice.models.User;
 import usermicroservice.services.UserService;
 import org.springframework.http.HttpStatus;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class UserController {
@@ -18,8 +20,22 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("api/users")
-    ResponseEntity<List<User>> returningAllUsers (){
-        return new ResponseEntity<>(userService.findAllUsers(), HttpStatus.OK);
+    @GetMapping("/api/users")
+    ResponseEntity<List<User>> gettingAllUsers (){
+        try {
+            return new ResponseEntity<>(userService.findAllUsers(), HttpStatus.OK);
+        }catch (Exception e){
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping("/api/users/{email}")
+    ResponseEntity<Optional<User>> gettingUserById(@PathVariable String email) {
+        try {
+            Optional<User> user = userService.findUserById(email);
+            return new ResponseEntity<>(user, HttpStatus.OK);
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
