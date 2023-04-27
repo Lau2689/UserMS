5feaa9362535fdd3931b7f04a4dd9264aa03dc59
Laura <lgal.martin@gmail.com>
2023-04-27 13:24:49 +0200

    resources, Product, Product.json

---
+++ src/main/java/usermicroservice/models/Product.java
@@ -0,0 +1,99 @@
+package usermicroservice.models;
+
+import javax.persistence.Column;
+
+import javax.persistence.Entity;
+
+import javax.persistence.GeneratedValue;
+
+import javax.persistence.GenerationType;
+
+import javax.persistence.Id;
+
+import javax.persistence.Table;
+
+import javax.validation.constraints.Min;
+
+import javax.validation.constraints.NotNull;
+
+import lombok.Builder;
+
+@Entity
+@Table(name = "product")
+@Builder
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "item_id")
+    private int itemId;
+    @NotNull
+    private String name;
+    @NotNull
+    private String description;
+    @NotNull
+    private double price;
+    @NotNull
+    @Min(0)
+    private int stock;
+
+    public Product(){}
+
+    public Product(int itemId, String name, String description, double price, int stock) {
+        this.itemId = itemId;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.stock = stock;
+    }
+
+    public int getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(int itemId) {
+        this.itemId = itemId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "itemId=" + itemId +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                ", stock=" + stock +
+                '}';
+    }
+}
--- src/main/java/usermicroservice/models/User.java
+++ src/main/java/usermicroservice/models/User.java
@@ -34,10 +34,9 @@ public class User {
     private String paymentMethod;
 
     private Integer fidelityPoints;
-
     private Double purchasePriceAverage;
     private Product favoriteproduct;
-    private Cart carts;
+
 
 
     public User() {}
